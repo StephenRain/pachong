b90cd8493a58701061b9a3df986f6c1bccc602bc
yaotianchi <yaotianchi@daojia-inc.com>
2020-04-05 18:50:07 +0800

    init

---
+++ src/main/java/com/puti/pachong/PachongApplication.java
@@ -0,0 +1,13 @@
+package com.puti.pachong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PachongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PachongApplication.class, args);
+    }
+
+}
