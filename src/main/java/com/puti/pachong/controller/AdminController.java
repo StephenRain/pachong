c3b22d4f67bc6df6ecac1a247c2bbf27fc8c4fd9
yaotianchi <yaotianchi@daojia-inc.com>
2020-04-05 21:15:24 +0800

    跳转通

--- src/main/java/com/puti/pachong/controller/AdminController.java
+++ src/main/java/com/puti/pachong/controller/AdminController.java
@@ -1,22 +1,37 @@
 package com.puti.pachong.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.puti.pachong.entity.Pachong;
 import com.puti.pachong.service.PachongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping(value = "admin")
 public class AdminController {
 
     @Autowired
     private PachongService pachongService;
 
-    @RequestMapping("/list")
-    public Object list(){
-        return pachongService.list();
+    @RequestMapping("/index")
+    public Object index(){
+        return new ModelAndView("index").addObject("name","张三");
     }
+
+    @RequestMapping("/list/view")
+    public Object list(){
+        List<Pachong> list = pachongService.list();
+        return new ModelAndView("pachong/list").addObject("list",list);
+    }
+
+
+
+
 
 
 }
